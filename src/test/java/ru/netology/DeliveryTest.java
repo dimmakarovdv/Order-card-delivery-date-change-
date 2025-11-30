950b28446fcfc7e947782297a0e282fc0a041ead
Neto test <dimmakarovdv89@mail.ru>
2025-11-30 22:14:51 +0500

    Added report generation via Allure

--- src/test/java/ru/netology/DeliveryTest.java
+++ src/test/java/ru/netology/DeliveryTest.java
@@ -1,8 +1,9 @@
 package ru.netology;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
@@ -13,45 +14,47 @@ import static com.codeborne.selenide.Selenide.open;
 
 class DeliveryTest {
 
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
     }
 
     @Test
-    @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
         DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
         int daysToAddForFirstMeeting = 4;
         String firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         int daysToAddForSecondMeeting = 7;
         String secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-
         $("[data-test-id=city] input").setValue(validUser.getCity());
-
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(firstMeetingDate);
-
         $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
         $("[data-test-id=agreement]").click();
         $(".button").click();
-
         $("[data-test-id=success-notification]")
                 .shouldBe(visible, Duration.ofSeconds(20))
                 .shouldHave(text("Успешно!"));
         $("[data-test-id=success-notification] .notification__content")
                 .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
-
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(".button").click();
-
         $("[data-test-id=replan-notification]")
                 .shouldBe(visible, Duration.ofSeconds(20))
                 .shouldHave(text("Необходимо подтверждение"));
         $("[data-test-id=replan-notification] .button").click();
-
         $("[data-test-id=success-notification]")
                 .shouldBe(visible, Duration.ofSeconds(20))
                 .shouldHave(text("Успешно!"));
